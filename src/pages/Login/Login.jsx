777901fa26e868884215ff33b6a5a7502dba19e4
oliver47 <oliver.gamarra47@gmail.com>
2024-08-19 18:47:06 -0400

    cambioo diseño login.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -89,6 +89,11 @@ const Login = () => {
         )
 
     }
+<<<<<<< HEAD
+=======
+
+    /*
+>>>>>>> 874a7b7 (cambioo diseño login.)
     return (
         <>
             {isLogin && session.type === 1 && <Navigate to={"/Dashboard"} replace={true} />}
@@ -120,7 +125,100 @@ const Login = () => {
                 </div>
             </div>
         </>
+    )^*/
+    return (
+        <>
+            {
+                user !== null
+                    ? user.type === 1
+                        ? <Navigate to={"/Dashboard"} replace={true} />
+                        : user.type === 2
+                            ? <Navigate to={"/Teacher/Home"} replace={true} />
+                            : user.type === 3
+                                ? <Navigate to={"/Admin"} replace={true} />
+                                : ''
+                    : ''
+            }
+
+
+            <Header />
+            <Content>
+  <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="sm:mx-auto sm:w-full sm:max-w-sm"> 
+    <img alt="Your Company"
+    src="/imagenes/logo.png" // Path from the public directory
+    className="mx-auto h-26 w-auto" // Adjust height and keep width auto
+        />
+      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white-900">
+        Iniciar Sesión
+      </h2>
+    </div>
+
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <Alert type={alert.type} message={alert.message} hide={hideAlert} />
+
+      <form action="/Dashboard" method="POST" className="space-y-6" onSubmit={(e) => submitHandle(e)}>
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium leading-6 text-white-900">
+            Correo electrónico
+          </label>
+          <div className="mt-2">
+            <Input
+              id="email"
+              name="email"
+              type="text"
+              required
+              autoComplete="email"
+              handle={emailHandle}
+              className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-white-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-center justify-between">
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-white-900">
+              Contraseña
+            </label>
+          
+          </div>
+          <div className="mt-2">
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              required
+              autoComplete="current-password"
+              handle={passwordHandle}
+              className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-white-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <Button
+            text='Login'
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          />
+        </div>
+        <div className="flex justify-center text-sm">
+              <a href="" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                Olvidaste tu contraseña?
+              </a>
+            </div>
+      </form>
+
+      
+    </div>
+  </div>
+</Content>
+</>
     )
+
+
+
+
+
 
 }
 
