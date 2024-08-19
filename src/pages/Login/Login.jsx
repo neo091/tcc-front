402ba8a0f2513cce144f4c7eea30a3d351afb5d7
neo091 <mneo666@gmail.com>
2024-08-20 01:59:39 +0200

    fix 2

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,77 +1,11 @@
-import { useState } from 'react'
-import Header from '../../components/Header'
-import isEmail from 'validator/lib/isEmail'
-import { doLogin } from '../../services/auth'
-import Alert from '../../components/Alerts'
-import { Link, Navigate, redirect } from 'react-router-dom'
-import Title from '../../components/Title'
-import { useAuthStore } from '../../store/authStore'
-import { ErrorType } from '../../services/helpers'
+import React from 'react';
 
-const Input = (props) => {
-    const { label, type, handle } = props
-    return (
-        <div className='my-6'>
-            <label htmlFor="email" className="block font-medium leading-6 text-white-900">
-                {label}
-            </label>
-            <input type={type} className='p-2 mt-2 bg-slate-700 rounded border-none w-full' onChange={(e) => handle(e.target.value)} />
-        </div>
-    )
-}
 const Login = () => {
-
-    const [hideAlert, setHideAlert] = useState(true)
-    const [alert, setAlert] = useState([])
-
-    const [email, setEmail] = useState(null)
-    const [password, setPassword] = useState(null)
-
-    const { session, isLogin, setSession, setIsLogin } = useAuthStore()
-
-    const emailHandle = (text) => setEmail(text)
-    const passwordHandle = (text) => setPassword(text)
-
-    const showAlert = ({ type, message, duration }) => {
-
-        setHideAlert(false)
-        setAlert({
-            type: type,
-            message: message
-        })
-
-        setTimeout(() => { setHideAlert(true) }, duration || 5000)
-    }
-    const LoginHandle = async (event) => {
-        event.preventDefault()
-
-        if (email) {
-            if (!isEmail(email)) {
-                showAlert({ type: ErrorType.DANGER, message: 'need a valid email please!' })
-                return
-            }
-        } else {
-            showAlert({ type: ErrorType.DANGER, message: 'need a email please!' })
-            return
-        }
-
-        if (!password) {
-            showAlert({ type: ErrorType.DANGER, message: 'need a password please!' })
-            return
-        }
-
-        if (isLogin) {
-            showAlert({ type: ErrorType.DANGER, message: 'is loggin!' })
-            return
-        }
-
-        const result = await doLogin({
-            email,
-            password
-        })
-    }
-
-
-
-    return (<></>)
+    return (
+        <div>
+            asd
+        </div>
+    );
 }
+
+export default Login;
