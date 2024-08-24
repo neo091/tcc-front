9858c6d311b8c231b7d602c53786d74dbf2c98ef
neo091 <mneo666@gmail.com>
2024-08-24 12:20:55 +0200

    add an separe counter, chart and chat sections

---
+++ src/components/ChatSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ChatSection = () => {
+    return (
+        <div className="grid md:grid-cols-6 gap-6 ">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+        </div>
+    );
+}
+
+export default ChatSection;
