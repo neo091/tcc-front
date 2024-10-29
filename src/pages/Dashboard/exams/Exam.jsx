a178d97999ccf03c25298e4ac8feb6c013b3508d
neo091 <mneo666@gmail.com>
2024-10-29 01:11:59 +0100

    Teacher: add exams types

--- src/pages/Dashboard/exams/Exam.jsx
+++ src/pages/Dashboard/exams/Exam.jsx
@@ -1,4 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { getAnalice } from "../../../services/gptService";
+import { CheckBadgeIcon, XCircleIcon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
 
 
 const Exam = () => {
@@ -8,6 +11,9 @@ const Exam = () => {
     const [currentQuestion, setCurrentQuestion] = useState(null)
     const [correctQuestion, setCorrectQuestion] = useState([])
     const [incorrectQuestion, setIncorrectQuestion] = useState([])
+    const [finalizado, setFinalizado] = useState(false)
+    const [notaExamen, setNotaExamen] = useState([])
+    const typingRef = useRef()
 
 
     const loadExam = async () => {
@@ -24,6 +30,19 @@ const Exam = () => {
     useEffect(() => {
         setCurrentQuestion(questions[current])
     }, [current])
+
+    const finishExam = async () => {
+        getAnalice({
+            correct: correctQuestion,
+            incorrect: incorrectQuestion
+        })
+            .then((response) => {
+                setFinalizado(true)
+                setNotaExamen(JSON.parse(response.body.res))
+                console.log(JSON.parse(response.body.res))
+            })
+            .catch((e) => console.error(e.message))
+    }
 
     const checkQuestion = (e) => {
 
@@ -45,16 +64,103 @@ const Exam = () => {
 
     }
 
+    const checkTyping = (e) => {
+
+        const typingText = typingRef.current.value
+        const currentText = typingText.toLowerCase()
+        const currentCorrect = currentQuestion?.correct.toLowerCase()
+
+        if (currentCorrect == currentText)
+            setCorrectQuestion([...correctQuestion, currentQuestion])
+        else
+            setIncorrectQuestion([...incorrectQuestion, currentQuestion])
+
+        typingRef.current.value = ""
+        const newCurrent = current + 1
+
+        if (newCurrent >= questions.length) {
+            setCurrentQuestion([])
+
+            finishExam()
+
+
+
+            return;
+        }
+
+        setCurrent(newCurrent)
+
+    }
+
     return (
-        <div className="flex items-center">
-            <div className="max-w-xl m-auto bg-slate-800 p-2">
-                <span>Corrects: {correctQuestion?.length} - Incorrect: {incorrectQuestion?.length}</span>
-                <h2 className="text-3xl">{currentQuestion?.ask}</h2>
-                {currentQuestion?.answers?.map((answer, index) => <button key={answer} onClick={checkQuestion} className={" bg-violet-600 hover:bg-violet-700 rounded block w-full text-center my-2 font-semibold text-white p-4 transition-all duration-500"}>
-                    {answer}
-                </button>)}
-            </div>
-        </div>
+        <>
+
+            {
+                currentQuestion?.type == "multiple_choice" && <div className="flex items-center">
+                    <div className="max-w-xl m-auto bg-slate-800 p-2">
+                        <h2 className="text-3xl">{currentQuestion?.ask}</h2>
+                        {currentQuestion?.answers?.map((answer, index) => <button key={answer} onClick={checkQuestion} className={" bg-violet-600 hover:bg-violet-700 rounded block w-full text-center my-2 font-semibold text-white p-4 transition-all duration-500"}>
+                            {answer}
+                        </button>)}
+                    </div>
+                </div>
+            }
+
+            {
+                currentQuestion?.type == "true_false" && <div className="flex items-center">
+                    <div className="max-w-xl m-auto bg-slate-800 p-2">
+                        <h2 className="text-3xl">{currentQuestion?.ask}</h2>
+                        {currentQuestion?.answers?.map((answer, index) => <button key={answer} onClick={checkQuestion} className={" bg-violet-600 hover:bg-violet-700 rounded block w-full text-center my-2 font-semibold text-white p-4 transition-all duration-500"}>
+                            {answer}
+                        </button>)}
+                    </div>
+                </div>
+            }
+
+            {
+                currentQuestion?.type == "typing" && <div className="flex items-center">
+                    <div className="max-w-xl m-auto bg-slate-800 p-2">
+                        <h2 className="text-3xl">{currentQuestion?.ask}</h2>
+                        <input type="text" placeholder="typing here!" ref={typingRef} className="w-full p-4 my-4 text-black" />
+                        <button onClick={checkTyping} className={" bg-violet-600 hover:bg-violet-700 rounded block w-full text-center my-2 font-semibold text-white p-4 transition-all duration-500"}>
+                            Aceptar
+                        </button>
+                    </div>
+                </div>
+            }
+
+            {
+                currentQuestion?.type == "typingImage" && <div className="flex items-center ">
+                    <div className="max-w-xl m-auto bg-slate-800 p-2 flex flex-col gap-4 ">
+                        <h2 className="text-3xl">{currentQuestion?.ask}</h2>
+                        <img src={currentQuestion?.src} alt="" className="rounded shadow" />
+                        <input type="text" placeholder="typing here!" ref={typingRef} className="w-full p-4 text-black" />
+                        <button onClick={checkTyping} className={" bg-violet-600 hover:bg-violet-700 rounded block w-full text-center font-semibold text-white p-4 transition-all duration-500"}>
+                            Aceptar
+                        </button>
+                    </div>
+                </div>
+            }
+
+
+            {
+                finalizado && <>
+
+                    <div className="text-center flex flex-col gap-3 items-center justify-center bg-slate-800 rounded max-w-[600px] m-auto py-8">
+
+                        {notaExamen.percentage > 80 ? <CheckBadgeIcon className="w-24 text-green-500" /> : <XCircleIcon className="w-24 text-red-600" />}
+                        <h2 className="text-2xl font-semibold">Has terminado el examen</h2>
+                        <p>Su puntuación: {notaExamen.totalPointsCorrect}/{notaExamen.totalPoints} ({notaExamen.percentage}%)</p>
+
+                    </div>
+
+                    <Link to={"/Dashboard/Exams"} className=" bg-violet-600 px-4 py-2 rounded w-[200px] m-auto block text-center mt-8">Volver</Link>
+
+                </>
+            }
+
+
+        </>
     );
 }
 
--- src/pages/Teacher/Exams/AddExam.jsx
+++ src/pages/Teacher/Exams/AddExam.jsx
@@ -12,6 +12,230 @@ export const loader = ({ params }) => {
     return { id: params.id }
 }
 
+
+const MultipleChoice = ({ item, examIndex, editAskHandle, editPointsHandle, editCorrectAnswerHandle, editAnswerHandle, removeAnswerHandle }) => {
+
+    const { id, ask, points } = item
+
+
+    return (
+        <>
+            <h3 className='text-3xl font-semibold'>Selección Multiple</h3>
+            <Card key={examIndex}>
+                <CardHeader>
+
+                    <CardTitle>
+                        {ask} <button className='inline-block p-2 bg-sky-600 rounded' onClick={() => editAskHandle(examIndex)}>
+                            <PencilIcon className='w-6 h-6' />
+                        </button>
+                    </CardTitle>
+                    <p className='flex gap-4 items-center'>
+                        Points:
+                        <button onClick={() => editPointsHandle(examIndex, "remove")} className='hover:bg-gray-700 transition-colors p-2 '><ChevronLeftIcon className='w-6 h-6' /></button>
+                        <span className='p-2'>{points}</span>
+                        <button onClick={() => editPointsHandle(examIndex, "add")} className='hover:bg-gray-700 transition-colors p-2'> <ChevronRightIcon className='w-6 h-6' /></button>
+                    </p>
+                </CardHeader>
+                <CardContent>
+                    <table>
+                        <tbody>
+                            {
+                                item.answers.map((answer, answerIndex) => {
+                                    return <tr key={`${answerIndex}-${answer}`}>
+                                        <td>
+
+                                            {
+                                                item.correct === answerIndex
+                                                    ? <span className='inline-block rounded w-6 h-6 bg-white cursor-pointer' onClick={() => editCorrectAnswerHandle(examIndex, answerIndex)}><CheckIcon className='w-6 h-6 text-green-600' /></span>
+                                                    : <span className='inline-block rounded w-6 h-6 bg-white cursor-pointer' onClick={() => editCorrectAnswerHandle(examIndex, answerIndex)}></span>
+                                            }
+
+                                        </td>
+                                        <td>
+                                            {answer}
+                                        </td>
+                                        <td>
+                                            <button className='inline-block p-2 bg-sky-600 rounded' onClick={() => editAnswerHandle(answerIndex, examIndex)}>
+                                                <PencilIcon className='w-6 h-6' />
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <button className='inline-block p-2 bg-red-600 rounded' onClick={() => removeAnswerHandle(examIndex, answerIndex)}>
+                                                <TrashIcon className='w-6 h-6' />
+                                            </button>
+                                        </td>
+                                    </tr>
+                                })
+                            }
+                        </tbody>
+                    </table>
+
+                </CardContent>
+            </Card>
+
+        </>
+    )
+}
+const TrueFalseItem = ({ item, examIndex, editAskHandle, editPointsHandle, editCorrectAnswerHandle }) => {
+
+    const { id, ask, points } = item
+
+    return (
+        <>
+            <h3 className='text-3xl font-semibold'>Falso Verdadero</h3>
+            <Card key={examIndex}>
+                <CardHeader>
+                    <CardTitle>
+                        {ask} <button className='inline-block p-2 bg-sky-600 rounded' onClick={() => editAskHandle(examIndex)}>
+                            <PencilIcon className='w-6 h-6' />
+                        </button>
+                    </CardTitle>
+                    <p className='flex gap-4 items-center'>
+                        Points:
+                        <button onClick={() => editPointsHandle(examIndex, "remove")} className='hover:bg-gray-700 transition-colors p-2 '><ChevronLeftIcon className='w-6 h-6' /></button>
+                        <span className='p-2'>{points}</span>
+                        <button onClick={() => editPointsHandle(examIndex, "add")} className='hover:bg-gray-700 transition-colors p-2'> <ChevronRightIcon className='w-6 h-6' /></button>
+                    </p>
+                </CardHeader>
+                <CardContent>
+                    <section className='flex gap-2'>
+                        {
+                            item.answers.map((answer, index) => {
+
+                                return (
+                                    <>
+
+                                        <article className='flex gap-2'>
+                                            {
+                                                item.correct === index
+                                                    ? <span className='inline-block rounded w-6 h-6 bg-white cursor-pointer' onClick={() => editCorrectAnswerHandle(examIndex, index)}><CheckIcon className='w-6 h-6 text-green-600' /></span>
+                                                    : <span className='inline-block rounded w-6 h-6 bg-white cursor-pointer' onClick={() => editCorrectAnswerHandle(examIndex, index)}></span>
+                                            }
+                                            <button>{answer}</button>
+                                        </article>
+                                    </>
+                                )
+                            })
+                        }
+                    </section>
+                </CardContent>
+            </Card >
+
+        </>
+    )
+}
+const Typing = ({ item, examIndex, editAskHandle, editPointsHandle, editCorrectAnswerHandle }) => {
+
+    const { id, ask, points } = item
+
+    const editCorrect = async () => {
+
+        const { value: answer } = await Swal.fire({
+            title: "Answer",
+            input: "text",
+            inputValue: item.correct,
+            inputLabel: "Your Answer",
+            inputPlaceholder: "Enter your Answer",
+
+        });
+        if (answer) {
+            // Swal.fire(`: ${answer}`);
+            editCorrectAnswerHandle(examIndex, answer)
+        }
+
+
+    }
+
+
+    return (
+        <>
+            <h3 className='text-3xl font-semibold'>Completa la oración</h3>
+            <Card key={examIndex}>
+                <CardHeader>
+                    <CardTitle>
+                        {ask} <button className='inline-block p-2 bg-sky-600 rounded' onClick={() => editAskHandle(examIndex)}>
+                            <PencilIcon className='w-6 h-6' />
+                        </button>
+                    </CardTitle>
+                    <p className='flex gap-4 items-center'>
+                        Points:
+                        <button onClick={() => editPointsHandle(examIndex, "remove")} className='hover:bg-gray-700 transition-colors p-2 '><ChevronLeftIcon className='w-6 h-6' /></button>
+                        <span className='p-2'>{points}</span>
+                        <button onClick={() => editPointsHandle(examIndex, "add")} className='hover:bg-gray-700 transition-colors p-2'> <ChevronRightIcon className='w-6 h-6' /></button>
+                    </p>
+                </CardHeader>
+                <CardContent>
+                    <section className='flex gap-2 items-center'>
+                        <span className='text-2xl'>{item.correct}</span>
+                        <button className='inline-block p-2 bg-sky-600 rounded' onClick={editCorrect}>
+                            <PencilIcon className='w-6 h-6' />
+                        </button>
+
+                    </section>
+                </CardContent>
+            </Card >
+        </>
+    )
+}
+const Image = ({ item, examIndex, editAskHandle, editPointsHandle, editCorrectAnswerHandle }) => {
+
+    const { id, ask, points } = item
+
+    const editCorrect = async () => {
+
+        const { value: answer } = await Swal.fire({
+            title: "Answer",
+            input: "text",
+            inputValue: item.correct,
+            inputLabel: "Your Answer",
+            inputPlaceholder: "Enter your Answer",
+
+        });
+        if (answer) {
+            // Swal.fire(`: ${answer}`);
+            editCorrectAnswerHandle(examIndex, answer)
+        }
+
+
+    }
+
+
+    return (
+        <>
+            <h3 className='text-3xl font-semibold'>Traduce la imagen</h3>
+            <Card key={examIndex}>
+                <CardHeader>
+                    <CardTitle>
+                        {ask} <button className='inline-block p-2 bg-sky-600 rounded' onClick={() => editAskHandle(examIndex)}>
+                            <PencilIcon className='w-6 h-6' />
+                        </button>
+                    </CardTitle>
+                    <p className='flex gap-4 items-center'>
+                        Points:
+                        <button onClick={() => editPointsHandle(examIndex, "remove")} className='hover:bg-gray-700 transition-colors p-2 '><ChevronLeftIcon className='w-6 h-6' /></button>
+                        <span className='p-2'>{points}</span>
+                        <button onClick={() => editPointsHandle(examIndex, "add")} className='hover:bg-gray-700 transition-colors p-2'> <ChevronRightIcon className='w-6 h-6' /></button>
+                    </p>
+                </CardHeader>
+                <CardContent>
+                    <section className=''>
+
+                        <img src={item?.src} alt="" />
+                        <div className='my-4 flex gap-4 items-center'>
+                            <span className='text-2xl '>{item.correct}</span>
+                            <button className='inline-block p-2 bg-sky-600 rounded' onClick={editCorrect}>
+                                <PencilIcon className='w-6 h-6' />
+                            </button>
+                        </div>
+
+                    </section>
+                </CardContent>
+            </Card >
+
+        </>
+    )
+}
+
 const ExamGeneratedList = (
     {
         saveHandle,
@@ -31,58 +255,27 @@ const ExamGeneratedList = (
                 {
                     examList.map((item, examIndex) => {
 
-                        const { id, ask, points } = item
+                        const { type } = item
+
                         return (
-                            <Card key={examIndex}>
-                                <CardHeader>
-                                    <CardTitle>
-                                        {ask} <button className='inline-block p-2 bg-sky-600 rounded' onClick={() => editAskHandle(examIndex)}>
-                                            <PencilIcon className='w-6 h-6' />
-                                        </button>
-                                    </CardTitle>
-                                    <p className='flex gap-4 items-center'>
-                                        Points:
-                                        <button onClick={() => editPointsHandle(examIndex, "remove")} className='hover:bg-gray-700 transition-colors p-2 '><ChevronLeftIcon className='w-6 h-6' /></button>
-                                        <span className='p-2'>{points}</span>
-                                        <button onClick={() => editPointsHandle(examIndex, "add")} className='hover:bg-gray-700 transition-colors p-2'> <ChevronRightIcon className='w-6 h-6' /></button>
-                                    </p>
-                                </CardHeader>
-                                <CardContent>
-                                    <table>
-                                        <tbody>
-                                            {
-                                                item.answers.map((answer, answerIndex) => {
-                                                    return <tr key={`${answerIndex}-${answer}`}>
-                                                        <td>
+                            <>
+                                {
+                                    type === "multiple_choice" && <MultipleChoice item={item} examIndex={examIndex} editAskHandle={editAskHandle} editPointsHandle={editPointsHandle} editCorrectAnswerHandle={editCorrectAnswerHandle} editAnswerHandle={editAnswerHandle} removeAnswerHandle={removeAnswerHandle} />
+                                }
 
-                                                            {
-                                                                item.correct === answerIndex
-                                                                    ? <span className='inline-block rounded w-6 h-6 bg-white cursor-pointer' onClick={() => editCorrectAnswerHandle(examIndex, answerIndex)}><CheckIcon className='w-6 h-6 text-green-600' /></span>
-                                                                    : <span className='inline-block rounded w-6 h-6 bg-white cursor-pointer' onClick={() => editCorrectAnswerHandle(examIndex, answerIndex)}></span>
-                                                            }
+                                {
+                                    type === "true_false" && <TrueFalseItem item={item} examIndex={examIndex} editAskHandle={editAskHandle} editPointsHandle={editPointsHandle} editCorrectAnswerHandle={editCorrectAnswerHandle} />
+                                }
 
-                                                        </td>
-                                                        <td>
-                                                            {answer}
-                                                        </td>
-                                                        <td>
-                                                            <button className='inline-block p-2 bg-sky-600 rounded' onClick={() => editAnswerHandle(answerIndex, examIndex)}>
-                                                                <PencilIcon className='w-6 h-6' />
-                                                            </button>
-                                                        </td>
-                                                        <td>
-                                                            <button className='inline-block p-2 bg-red-600 rounded' onClick={() => removeAnswerHandle(examIndex, answerIndex)}>
-                                                                <TrashIcon className='w-6 h-6' />
-                                                            </button>
-                                                        </td>
-                                                    </tr>
-                                                })
-                                            }
-                                        </tbody>
-                                    </table>
+                                {
+                                    type === "typing" && <Typing item={item} examIndex={examIndex} editAskHandle={editAskHandle} editPointsHandle={editPointsHandle} editCorrectAnswerHandle={editCorrectAnswerHandle} />
+                                }
 
-                                </CardContent>
-                            </Card>
+                                {
+                                    type === "typingImage" && <Image item={item} examIndex={examIndex} editAskHandle={editAskHandle} editPointsHandle={editPointsHandle} editCorrectAnswerHandle={editCorrectAnswerHandle} />
+                                }
+                            </>
+
                         )
                     })
                 }
@@ -111,13 +304,48 @@ const AddExam = () => {
         const updates = Object.fromEntries(formData);
 
         // console.log(updates);
-        const myExam = await generateExam(updates);
+        // const myExam = await generateExam(updates);
 
-        const { body } = myExam;
+        // const { body } = myExam;
 
 
-        setExam(body.asks);
-        setSaveData(updates);
+        setExam([
+            {
+                "ask": "¿Cómo te presentarías en inglés?",
+                "answers": [
+                    "My name is Juan.",
+                    "I am fine.",
+                    "He is happy."
+                ],
+                "correct": 0,
+                "type": "multiple_choice",
+                "points": 1
+            },
+            {
+                "ask": "La frase correcta para responder a 'Nice to meet you' es 'Nice to meet you, too.'",
+                "answers": [
+                    "Verdadero",
+                    "Falso"
+                ],
+                "correct": 0,
+                "type": "true_false",
+                "points": 2
+            },
+            {
+                "ask": "Completa correctamente: 'Nice to meet you, ___.'",
+                "correct": "Too",
+                "type": "typing",
+                "points": 2
+            },
+            {
+                "ask": "Traduce al español el texto de la imagen",
+                "src": "http://localhost:5173/images/exams/sunny.png",
+                "correct": "Soleado",
+                "type": "typingImage",
+                "points": 2
+            },
+        ]);
+        // setSaveData(updates);
     }
 
     const editAnswerHandle = async (answerIndex, examIndex) => {
@@ -254,7 +482,7 @@ const AddExam = () => {
                     </div>
                 </div>
             </div>
-            <div className='flex flex-col gap-2 mt-4'>
+            <div className='flex flex-col gap-6 mt-4 max-w-[720px] m-auto'>
                 <ExamGeneratedList
                     editAnswerHandle={editAnswerHandle}
                     editAskHandle={editAskHandle}
