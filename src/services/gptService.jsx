962418e9cbaf36f484bd283d4406448981d4a6f6
neo091 <mneo666@gmail.com>
2024-11-14 02:07:12 +0100

    add: chatbot gpt service

--- src/services/gptService.jsx
+++ src/services/gptService.jsx
@@ -1,6 +1,23 @@
+import { getUserData } from '../auth'
 import axios from 'axios'
 
-const base_url = "http://localhost:4000/api/gpt"
+const base_url = "http://localhost:4000/api"
+
+let token = null
+
+const setToken = newToken => {
+    token = `Bearer ${newToken}`
+}
+
+
+async function getSession() {
+    const user = await getUserData()
+
+    setToken(user.token)
+
+}
+await getSession()
+
 
 export const getResume = async (resume_object) => {
     const data = JSON.parse(resume_object)
@@ -17,7 +34,7 @@ export const getRecommendations = async (resume_object) => {
 
 
     const response = await axios
-        .post(`${base_url}/recomendations`, data)
+        .post(`${base_url}/gpt/recomendations`, data)
 
     return response.data
 }
@@ -25,14 +42,14 @@ export const getRecommendations = async (resume_object) => {
 export const getQuestions = async () => {
 
     const response = await axios
-        .get(`${base_url}/test-connect`, {})
+        .get(`${base_url}/gpt/test-connect`, {})
 
     return response.data
 }
 
 export const generateExam = async (data) => {
     const response = await axios
-        .post(`${base_url}/generate/exam`, data)
+        .post(`${base_url}/gpt/generate/exam`, data)
 
     return response.data
 
@@ -40,6 +57,16 @@ export const generateExam = async (data) => {
 
 export async function getAnalice(data) {
     const response = await axios
-        .post(`${base_url}/exam/analice`, data)
+        .post(`${base_url}/gpt/exam/analice`, data)
+    return response.data
+}
+
+export async function sendMessageService(message) {
+
+
+    console.log(token)
+    const response = await axios.post(`${base_url}/dashboard/chatbot/send-message`, { message: message }, {
+        headers: { Authorization: token }
+    })
     return response.data
 }
